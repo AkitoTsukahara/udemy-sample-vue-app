24a9a8a150c83f9f125af85a128e747f756d1588
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-01-10 20:03:43 +0900

    edit router

--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,15 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    // Homeページ
     path: '/',
     name: 'Home',
     component: Home
   },
   {
+    // Aboutページ
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
